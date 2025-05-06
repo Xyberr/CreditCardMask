37c1578468a9ba0d7eeb38f8584b0691c947ef5c
Xyberr <makszacev238@gmail.com>
2025-05-06 19:34:18 +0500

    Added Main to the CreditCardMask project

---
+++ Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String inputStr;
+        String outStr;
+        int UnencryptedNumbers;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число, сколько нужно оставить в конце незашиврованных чисел: ");
+        UnencryptedNumbers = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Введите цифры: ");
+        inputStr = scanner.nextLine();
+        scanner.close();
+
+        StringBuilder sb = new StringBuilder(inputStr);
+
+        /*Сам шифровщик*/
+        for (int i = 0; i < inputStr.length() - UnencryptedNumbers; i++){
+            sb.setCharAt(i, '#');
+        }
+        outStr = sb.toString();
+
+        System.out.printf("Ваши зашифрованные данные: %s", outStr);
+    }
+}
